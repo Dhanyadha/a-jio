02d0db0eb33f011d0a581a0776e507265f7c9558
Dhanyadha <156797407+Dhanyadha@users.noreply.github.com>
2025-07-07 10:26:12 +0530

    Update categoriesSlice.js

--- src/Redux/Slice/categoriesSlice.js
+++ src/Redux/Slice/categoriesSlice.js
@@ -1,12 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  items: [
-    { id: 1, name: 'Women Wear', image: '/images/women.jpg' },
-    { id: 2, name: 'Men Wear', image: '/images/men.jpg' },
-    { id: 3, name: 'Kids Wear', image: '/images/kids.jpg' },
-  ],
-};
 
 const categoriesSlice = createSlice({
   name: 'categories',
