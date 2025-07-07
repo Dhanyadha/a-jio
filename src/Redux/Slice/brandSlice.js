cff149241c3907b2f7d9fb5a674f54a0376b41fd
Dhanyadha <156797407+Dhanyadha@users.noreply.github.com>
2025-07-07 10:25:51 +0530

    Update brandSlice.js

--- src/Redux/Slice/brandSlice.js
+++ src/Redux/Slice/brandSlice.js
@@ -1,13 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  items: [
-    { id: 1, name: 'GAP', image: '/images/brand1.jpg' },
-    { id: 2, name: 'Levis', image: '/images/brand2.jpg' },
-    { id: 3, name: 'Aarong', image: '/images/brand3.jpg' },
-    { id: 4, name: 'Zara', image: '/images/brand4.jpg' },
-  ],
-};
+
 
 const brandsSlice = createSlice({
   name: 'brands',
